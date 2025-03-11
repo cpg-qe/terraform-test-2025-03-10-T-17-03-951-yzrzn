d97a14b315a1c05e426be774fcf403632fa2484c
cpg-qe <swathi.g@servicenow.com>
2025-03-11 09:39:07 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscriptionId
+  client_id       = var.clientId
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantId
+  features {}
+}
